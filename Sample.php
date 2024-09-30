3b521ea08f060a3e93010aafa33283ffed0267b9
SuprajaKonchada <supraja.konchada@cognine.com>
2024-09-30 13:27:06 +0530

    first commit

---
+++ Sample.php
@@ -0,0 +1,13 @@
+
+a= array(1,2,3,4);
+b= array(3,2,4,5);
+for($i=0;$i<lenght(a);$i++){
+    for($j=0;$j<lenght(b);$j++){
+        if($a[i] == $b[j])
+        {
+            echo $a[i]
+        }
+    }
+
+    }
+}
